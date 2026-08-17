3d0f32c5197f8c8ce59a512d0ba1fcf1976f45ba
VIRENDRA PANWAR <virendrapanwar@VIRENDRAs-Laptop.local>
2026-08-17 21:41:19 +0530

    Add 8.2 C program

---
+++ 8.2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter 3 numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b && a >= c) {
+        printf("%d is largest\n", a);
+    } 
+    else if (b >= a && b >= c) {
+        printf("%d is largest\n", b);
+    } 
+    else {
+        printf("%d is largest\n", c);
+    }
+
+    return 0;
+}
